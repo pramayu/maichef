e1437e8d96030e24489adb96b5b3ead332f1efa9
pramayu <gedepramayu@gmail.com>
2020-05-01 22:50:12 +0800

    setup graphl view

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from app.api import schema
+from flask_graphql import GraphQLView
 from flask_mongoengine import MongoEngine
 from app.config.conf import DevelopmentConf
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConf)
 db = MongoEngine(app)
+
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
