b8f2499894f4bb31e491e45adedbd35c02060ed7
pramayu <gedepramayu@gmail.com>
2020-05-04 00:23:05 +0800

    remove list chef experience

--- app/api/service/chef/__init__.py
+++ app/api/service/chef/__init__.py
@@ -2,8 +2,10 @@ import graphene as grap
 from app.api.service.chef.be_chef import ToBeChef
 from app.api.service.chef.experience import PushExperience
 from app.api.service.chef.experience import EditExperience
+from app.api.service.chef.experience import PullExperience
 
 class ChefServ(grap.ObjectType):
 	tobechef 			= ToBeChef.Field()
 	pushexperience 		= PushExperience.Field()
 	editexperience 		= EditExperience.Field()
+	pullexperience 		= PullExperience.Field()
